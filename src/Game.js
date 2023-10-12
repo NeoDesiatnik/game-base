2946360d1dfcb4ca54053b1f32d9826a11e8f5b6
NeoDesiatnik <neobackstrom.desiatnik@elev.ga.ntig.se>
2023-10-12 11:09:33 +0200

    Hela koden + håller på att lägga till powerups

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,94 @@
+import Slime from './Slime.js'
+import PowerSlime from './PowerSlime.js'
+import InputHandler from './InputHandler.js'
+import Player from './Player.js'
+import UserInterface from './UserInterface.js'
 export default class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
+    this.input = new InputHandler(this)
+    this.ui = new UserInterface(this)
     this.keys = []
-    this.enemies = []
     this.gameOver = false
     this.gravity = 1
     this.debug = false
+    this.gameTime = 0
+
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 1000
+    this.powerupTimer = 0
+    this.powerupInterval = 10
+
+    this.player = new Player(this)
   }
 
   update(deltaTime) {
     if (!this.gameOver) {
       this.gameTime += deltaTime
     }
+    this.player.update(deltaTime)
+
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+      this.addEnemy()
+      this.enemyTimer = 0
+    } else {
+      this.enemyTimer += deltaTime
+    }
+    if (this.powerupTimer > this.powerupInterval && !this.gameOver) {
+      this.addPowerup()
+      this.powerupTimer = 0
+    }
+
+    this.enemies.forEach((enemy) => {
+      enemy.update(deltaTime)
+      if (this.checkCollision(this.player, enemy)) {
+        enemy.markedForDeletion = true
+      }
+      this.player.projectiles.forEach((projectile) => {
+        if (this.checkCollision(projectile, enemy)) {
+          enemy.markedForDeletion = true
+          projectile.markedForDeletion = true
+        }
+      })
+    })
+
+    this.enemies.forEach((powerup) => {
+      powerup.update(deltaTime)
+      if (this.checkCollision(this.player, powerup)) {
+        powerup.markedForDeletion = true
+      }
+      this.player.projectiles.forEach((projectile) => {
+        if (this.checkCollision(projectile, powerup)) {
+          powerup.markedForDeletion = true
+          projectile.markedForDeletion = true
+        }
+      })
+    })
+    this.enemies = this.enemies.filter((powerup) => !powerup.markedForDeletion)
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
   }
 
-  draw(context) {}
+  draw(context) {
+    this.ui.draw(context)
+    this.player.draw(context)
+    this.enemies.forEach((enemy) => enemy.draw(context))
+  }
+
+  addEnemy() {
+    this.enemies.push(new Slime(this))
+  }
+  addPowerup() {
+    this.powerups.push(new PowerSlime(this))
+  }
+
+  checkCollision(object1, object2) {
+    return (
+      object1.x < object2.x + object2.width &&
+      object1.x + object1.width > object2.x &&
+      object1.y < object2.y + object2.height &&
+      object1.height + object1.y > object2.y
+    )
+  }
 }
